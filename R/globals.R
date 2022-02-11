011a6e18a5df4fbc8753f41cebd2d012823d8028
ilizaga <30837036+ivanlizaga@users.noreply.github.com>
2022-02-11 15:41:03 +0100

    Delete R directory

--- R/globals.R
+++
@@ -1,3 +0,0 @@
-utils::globalVariables(c("LD1", "LD2","LD3","group_number","Sources","row_num","xvar", "yvar", 
-                         "varname", "angle", "hjust","Dw1","Dw2","Dw3","value","variable",
-                         "..scaled..","s_max","t_max","s_min","t_min","Dw4","red","groups"))
